05a038a5df72df086ff6803c5cf65211b212c35e
Nzhaer7 <suleymanogrk24@gmail.com>
2023-05-20 12:51:18 +0300

    Scraper basic tempalate update

--- ScrapData.py
+++ ScrapData.py
@@ -8,14 +8,26 @@ url=""
 
 driver = webdriver.Chrome("./chromedriver.exe")
 
+Data1=[]
+
+Data2=[]
+
+Data3=[]
+
+driver.get(url)
+
 content=driver.page_source
 
 soup=BeautifulSoup(content)
 
-df=pd.DataFrame()
+for i in soup.findAll('i', href=True, attrs={'class':''}):
+    data1=i.find('div', attrs={'class':''})
+    data2=i.find('div', attrs={'class':''})
+    data3=i.find('div', attrs={'class':''})
+    Data1.append(data1.text)
+    Data2.append(data2.text)
+    Data3.append(data3.text)
+
+df=pd.DataFrame({'coulmn_name':Data1,'coulmn_name':Data2,'column_name':Data3})
 
 df.to_csv('data.csv', index=False, encoding='utf-8')
-
-data=[]
-
-driver.get(url)
